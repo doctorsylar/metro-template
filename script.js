8f16acc0449079e7e771cb786f153444de2f8c55
Andrey <soul.evans.15@gmail.com>
2018-09-15 20:16:04 +0300

    pre-finish commit

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+'use strict';
 $(function () {
+    // MENU TOGGLE
     $('div.menu-toggler > i').click(function () {
         $('#menu-placeholder').toggleClass('show');
+        $('.menu-toggler > i').toggleClass('fa-bars');
+        $('.menu-toggler > i').toggleClass('fa-times');
+    });
+    // CAROUSEL
+    let carousel = $('.owl-carousel');
+    carousel.owlCarousel({
+        items: 1,
+        loop: true,
+        nav: false,
+        dots: false
+    });
+    $('.arrow-item.left').click(function () {
+        carousel.trigger('prev.owl.carousel');
+    });
+    $('.arrow-item.right').click(function () {
+        carousel.trigger('next.owl.carousel');
     });
 });
